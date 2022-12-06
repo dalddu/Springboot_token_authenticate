af584c9d1e02234fc2ec40b1076a1580dd45d614
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-12-06 18:04:12 +0900

    Controller에서 UserName꺼내기

--- src/main/java/com/example/tokenauthenticate/service/ReviewService.java
+++ src/main/java/com/example/tokenauthenticate/service/ReviewService.java
@@ -1,13 +1,12 @@
 package com.example.tokenauthenticate.service;
 
+import com.example.tokenauthenticate.utils.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 @Service
 public class ReviewService {
 
-
-
-    public String write() {
-        return "리뷰가 등록되었습니다.";
+    public String write(String userName) {
+        return userName + "님의 리뷰가 등록되었습니다.";
     }
 }
