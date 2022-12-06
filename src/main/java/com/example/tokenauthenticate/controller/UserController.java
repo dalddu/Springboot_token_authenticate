89aa3ce45d8d11c110e9f0b6dbcef9e3fa6e5ef0
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-12-06 11:46:07 +0900

    로그인 기능 추가

---
+++ src/main/java/com/example/tokenauthenticate/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.tokenauthenticate.controller;
+
+
+import com.example.tokenauthenticate.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(){
+        return ResponseEntity.ok().body(userService.login("", ""));
+    }
+}
